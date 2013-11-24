29e232266d42eed3060d13731d35d4887b7f5b58
tanechka <gvardi0@gmail.com>
2013-11-24 13:35:19 -0500

    asd

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,5 +1,5 @@
 class Album < ActiveRecord::Base
    validates :title, presence: true
-   has_many :images
+   has_many :images, dependend: :destroy
    validates :title, length: { maximum: 25 }
 end
